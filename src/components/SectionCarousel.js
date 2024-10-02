ac7b85a0ef23c352e6416fc459a7b6ab6d5934d6
John Patrick Bautista <jpc.bautista1@gmail.com>
2024-10-02 19:32:25 +0800

    Initial commit

---
+++ src/components/SectionCarousel.js
@@ -0,0 +1,45 @@
+import * as React from 'react';
+import { alpha, Paper } from '@mui/material';
+import Button from '@mui/material/Button';
+import Carousel from 'react-material-ui-carousel'
+
+export default function SectionCarousel() {
+
+  var items = [
+    {
+        name: "",
+        description: "Mitsubishi Triton",
+        src: require("../assets/images/1.jpg")
+    },
+    {
+        name: "",
+        description: "Kia Sonet",
+        src: require("../assets/images/2.jpg")
+    }
+  ]
+
+  return (
+    <div>
+        <Carousel>
+        {
+            items.map( (item, i) => <Item key={i} item={item} /> )
+        }
+        </Carousel>
+    </div>
+  );
+
+  function Item(props) 
+  {
+      return (
+          <Paper>
+              <h2>{props.item.name}</h2>
+              <img src={props.item.src} alt={props.item.description}/>
+
+              <Button className="CheckButton">
+                  {props.item.description} 
+              </Button>
+          </Paper>
+      )
+  }
+
+}
---
+++ src/components/SectionCarousel2.js
@@ -0,0 +1,53 @@
+import * as React from 'react';
+import { alpha, Paper, Grid } from '@mui/material';
+import Button from '@mui/material/Button';
+import Carousel from 'react-material-ui-carousel'
+
+export default function SectionCarousel2() {
+
+  var items = [
+    {
+        name: "",
+        description: "Hyundai Stargazer",
+        src: require("../assets/images/3.jpg")
+    },
+    {
+        name: "",
+        description: "Peugeot 2008",
+        src: require("../assets/images/4.jpg")
+    },
+    {
+        name: "",
+        description: "Chevrolet Trailblazer",
+        src: require("../assets/images/5.jpg")
+    }
+  ]
+
+  return (
+    <div>
+        <Carousel>
+        {
+            <Item />
+            // items.map( (item, i) => <Item key={i} item={item} /> )
+        }
+        </Carousel>
+    </div>
+  );
+
+  function Item(props) 
+  {
+      return (
+          <Paper>
+            <Grid container spacing={2} padding={2}>
+                {
+                    items.map( (item, i) => 
+                        <Grid item xs={4}>
+                            <img src={item.src} alt={item.description}/>
+                        </Grid> )
+                }
+            </Grid>
+          </Paper>
+      )
+  }
+
+}
